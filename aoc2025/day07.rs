6fe4cb48b0347df6c5a41e3f2e83764cb269410c
Molly Litherland <mlitherland@duck.com>
2025-12-08 15:25:42 -0500

    Day 7 complete, day 8 started

--- aoc2025/day07.rs
+++ aoc2025/day07.rs
@@ -1,7 +1,59 @@
+use std::collections::HashMap;
 use std::fs;
 
 fn main() {
-    let file = fs::read_to_string("day07.test").unwrap();
+    let file = fs::read_to_string("day07.dat").unwrap();
 
-    let mut beam_columns: Vec<u64> = vec![];
+    let mut beam_columns: HashMap<usize, bool> = HashMap::new();
+    let mut count = 0;
+    
+    let mut lines = file.lines();
+    let first_line = lines.next().unwrap();
+    for (i, c) in first_line.char_indices() {
+        if c == 'S' {
+            beam_columns.insert(i, true);
+        }
+    }
+    println!("{}", first_line);
+
+    while let Some(line) = lines.next() {
+        let chars: Vec<char> = line.chars().collect();
+
+        for i in 0..chars.len() {
+            let mut beam = false;
+            if let Some(_) = beam_columns.get(&i) {
+                beam = true;
+            }
+
+            if *chars.get(i).unwrap() == '^' {
+                if beam {
+                    count += 1;
+                    beam_columns.remove(&i);
+                    if i > 0 {
+                        let prev = chars.get(i - 1).unwrap();
+                        if *prev != '^' {
+                            beam_columns.insert(i - 1, true);
+                        }
+                    }
+                    if i < chars.len() - 1 {
+                        let next = chars.get(i + 1).unwrap();
+                        if *next != '^' {
+                            beam_columns.insert(i + 1, true);
+                        }
+                    }
+                }
+            }
+        }
+
+        for i in 0..chars.len() {
+            if let Some(_) = beam_columns.get(&i) {
+                print!("|");
+            } else {
+                print!("{}", chars.get(i).unwrap());
+            }
+        }
+        println!();
+    }
+
+    println!("Count: {}", count);
 }
